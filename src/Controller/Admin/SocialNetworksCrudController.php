446300a84ecf1dc99d32b368d798c999ff5e2a86
Michael Ballester <mike@mikebgdev.com>
2024-04-17 17:14:36 +0200

    php cs fixer

--- src/Controller/Admin/SocialNetworksCrudController.php
+++ src/Controller/Admin/SocialNetworksCrudController.php
@@ -1,14 +1,17 @@
 <?php
+/*
+ * This class is part of a software application developed by Michael Ballester Granero.
+ *
+ * The application is distributed under the terms of the MIT License.
+ * For more information, please see the LICENSE file included in the source code.
+ */
 
 namespace App\Controller\Admin;
 
 use App\Entity\SocialNetworks;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
